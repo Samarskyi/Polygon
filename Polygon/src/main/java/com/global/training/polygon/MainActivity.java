d1747d370cb3fb0f37b1aebac0937dedb2e1e97c
eugenii.samarskyi <e.samarskyi@gmail.com>
2014-11-12 17:35:44 +0200

    Works with auth

--- Polygon/src/main/java/com/global/training/polygon/MainActivity.java
+++ Polygon/src/main/java/com/global/training/polygon/MainActivity.java
@@ -10,15 +10,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URI;
 
 public class MainActivity extends Activity {
 
@@ -63,7 +67,17 @@ public class MainActivity extends Activity {
 
 			HttpClient httpclient = new DefaultHttpClient();
 
+
+
 			HttpGet httpGet = new HttpGet(url);
+
+//			httpGet.setURI(url);
+			httpGet.addHeader(BasicScheme
+					.authenticate(new UsernamePasswordCredentials("eugenii.samarskyi", "[pi989898pi]!"),"UTF-8", false));
+//			UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("eugenii.samarskyi", "[pi989898pi]!");
+//			BasicScheme scheme = new BasicScheme();
+//			Header authorizationHeader = scheme.authenticate(credentials, httpGet);
+//			httpGet.addHeader(authorizationHeader);
 
 			HttpResponse httpResponse = httpclient.execute(httpGet);
 
